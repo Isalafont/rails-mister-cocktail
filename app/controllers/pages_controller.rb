197d9f0019da6f88912e06f6bfaf3d8cc08652f6
Isabelle Lafont <isalafont@gmail.com>
2020-06-30 14:52:06 +0200

    try to fix bug end up to fix index cocktails page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home, :index, :show]
 
   # def home
   #   redirect_to cocktails_path unless current_user.nil?
--- app/policies/cocktail_policy.rb
+++ app/policies/cocktail_policy.rb
@@ -1,11 +1,28 @@
 class CocktailPolicy < ApplicationPolicy
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    user == record.user
+  end
+
   class Scope < Scope
     def resolve
       scope.all
     end
-  end
-
-  def create?
-    return true
   end
 end
